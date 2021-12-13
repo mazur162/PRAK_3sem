2cceb064f26e891a4b5caa4b9a4b742aefd0037e
mazur162 <mazur.anastasia162@gmail.com>
2021-12-13 22:14:27 +0300

    Working on shell utilits

---
+++ P/P041 (utilits)/cmp.c
@@ -0,0 +1,140 @@
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <stdio.h>
+
+/*
+    cmp filename1 filename2
+
+   ОШИБКИ: 
+    
+    1: Отсутствует файл (или оба файла)
+    2: Слишком большое количество аргументов для вызова cp
+    3: Файл не существует
+    4: Ошибка записи
+
+    КОД ВОЗВРАТА ВСЕГДА 1 ПРИ ОШИБКЕ
+    (как в оригинальном cmp)
+*/
+
+enum {BUF_SIZE = 208};
+
+ssize_t
+min(ssize_t a, ssize_t b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int
+open_file(int *fd, char *filename) {
+    *fd = open(filename, O_RDONLY);
+    if (*fd == -1) {
+        const char* NO_FILE = "cp: No such file or directory\n";
+        if (write(2, NO_FILE, strlen(NO_FILE)) != strlen(NO_FILE)) {
+            exit(1);
+        }
+        exit(1);
+    }
+    return 0;
+}
+
+int
+num_symbols(unsigned long num){
+    int count = 0;
+    while (num == 0) {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+int
+different (int byte_num, int line_num, int *fd1, int *fd2, char *s1, char *s2) {
+    char byte_num_str[byte_num / 10 + 1];
+    itoa(byte_num, byte_num_str, 10);
+
+    char line_num_str[line_num / 10 + 1];
+    itoa(line_num, line_num_str, 10);
+
+    char *MSG = malloc(sizeof(*MSG) * (strlen(s1) + strlen(s2) + strlen(byte_num_str) + strlen(line_num_str) + 25));
+    strcpy(MSG, s1);
+    strcat(MSG, " ");
+    strcat(MSG, s2);
+    strcat(MSG, " differ: char ");
+    strcat(MSG, byte_num_str);
+    strcat(MSG, ", line ");
+    strcat(MSG, line_num_str);
+    strcat(MSG, "\n");
+
+    if (write(2, MSG, strlen(MSG))  != strlen(MSG)) {
+        exit(1);
+    }
+    free(MSG);
+
+    close(*fd1);
+    close(*fd2);
+    return 0;
+}
+
+int
+main(int argc, char **argv) {
+    if (argc < 3) {
+        const char *MISSING_OP = "cmp: missing file(s)\n";
+        if (write(2, MISSING_OP, strlen(MISSING_OP)) != strlen(MISSING_OP)) {
+            exit(1);
+        }
+        exit(1);
+    }
+
+    if (argc > 3) {
+        const char *TOO_MANY_ARGS = "cmp: too many arguments\n";
+        if (write(2, TOO_MANY_ARGS, strlen(TOO_MANY_ARGS)) != strlen(TOO_MANY_ARGS)) {
+            exit(1);
+        }
+        exit(1);
+    }
+    int fd1, fd2, res;
+    if ((res = open_file(&fd1, argv[1])) != 0) {
+        return res;
+    }
+    if ((res = open_file(&fd2, argv[2])) != 0) {
+        close(fd1);
+        return res;
+    }
+
+    char buf1[BUF_SIZE], buf2[BUF_SIZE];
+    unsigned long byte_num = 1, line_num = 1;
+    ssize_t n1, n2;
+    int flag = 0;
+    while ((n1 = read(fd1, buf1, BUF_SIZE)) > 0 &&
+            (n2 = read(fd2, buf2, BUF_SIZE)) > 0) {
+        flag = 1;
+        for (int i = 0; i < min(n1, n2); i++) {
+            if ((char)buf1[i] != (char)buf2[i]) {
+                return different(byte_num, line_num, &fd1, &fd2, argv[1], argv[2]);
+            }
+            byte_num++;
+            if ((char)buf1[i] == '\n')
+                line_num++;
+        }
+        if (n1 != n2) {
+            return different(byte_num + 1, line_num, &fd1, &fd2, argv[1], argv[2]);
+        }
+    }
+    
+    if (((n1 == 0 && (n2 = read(fd2, buf2, BUF_SIZE)) > 0) ||
+            (n2 == 0 && n1 > 0)) && (flag == 1)) {
+        return different(byte_num, line_num, &fd1, &fd2, argv[1], argv[2]);
+    }
+
+    close(fd1);
+    close(fd2);
+    return 0;
+}
---
+++ P/P041 (utilits)/cp.c
@@ -0,0 +1,111 @@
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+/*
+    cp old_file new_file
+
+    ОШИБКИ: 
+    
+    1: Отсутствует файл (или оба файла)
+    2: Слишком большое количество аргументов для вызова cp
+    3: old_file не существует
+    4: Ошибка записи
+    5: Ошибка при вызове stat (права доступа)
+    6: Ошибка при открытии файла
+    7: Ошибка чтения
+    8: Файлы идентичны (один и тот же файл)
+
+    КОД ВОЗВРАТА ВСЕГДА 1 ПРИ ОШИБКЕ
+    (как в оригинальном cp)
+*/
+
+enum {BUF_SIZE = 208};
+
+int
+main(int argc, char **argv)
+{
+    if (argc < 3) {
+        const char *MISSING_OP = "cp: missing file(s)\n";
+        if (write(2, MISSING_OP, strlen(MISSING_OP)) != strlen(MISSING_OP)) {
+            exit(1);
+        }
+        exit(1);
+    }
+
+    if (argc > 3) {
+        const char *TOO_MANY_ARGS = "cp: too many arguments\n";
+        if (write(2, TOO_MANY_ARGS, strlen(TOO_MANY_ARGS)) != strlen(TOO_MANY_ARGS)) {
+            exit(1);
+        }
+        exit(1);
+    }
+
+    int old_file_fd = open(argv[1], O_RDONLY);
+    if (old_file_fd == -1) {
+        char* NO_FILE = malloc(sizeof(*NO_FILE) * (strlen(argv[1]) + 30));
+        strcpy(NO_FILE, "cp: ");
+        strcat(NO_FILE, argv[1]);
+        strcat(NO_FILE, ": No such file or directory\n");
+        if (write(2, NO_FILE, strlen(NO_FILE)) != strlen(NO_FILE)) {
+            exit(1);
+        }
+        free(NO_FILE);
+        exit(1);
+    }
+
+    struct stat file1_s;
+    if (stat(argv[1], &file1_s) == -1) {
+        close(old_file_fd);
+        exit(1);
+    }
+
+    int new_file_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file1_s.st_mode);
+    if (new_file_fd == -1) {
+        close(old_file_fd);
+        exit(1);
+    }
+
+    struct stat file2_s;
+    if (stat(argv[2], &file2_s) == -1) {
+        close(old_file_fd);
+        exit(1);
+    }
+
+    if (file1_s.st_ino == file2_s.st_ino) {
+        char* IDENT_FILES = malloc(sizeof(*IDENT_FILES) * (strlen(argv[1]) + strlen(argv[2]) + 39));
+        strcpy(IDENT_FILES, "cp: ");
+        strcat(IDENT_FILES, argv[1]);
+        strcat(IDENT_FILES, " and ");
+        strcat(IDENT_FILES, argv[2]);
+        strcat(IDENT_FILES, " are identical (not copied).\n");
+        if (write(2, IDENT_FILES, strlen(IDENT_FILES))  != strlen(IDENT_FILES)) {
+            exit(1);
+        }
+        free(IDENT_FILES);
+        exit(1);
+    }
+
+    char buf[BUF_SIZE];
+    ssize_t n;
+    while ((n = read(old_file_fd, buf, BUF_SIZE)) > 0) {
+        if (write (new_file_fd, buf, n) != n) {
+            close(old_file_fd);
+            close(new_file_fd);
+            exit(1);
+        }
+    }
+    if (errno != 0) {
+        const char *READ_ERR = "cp: error while reading\n";
+        if (write(2, READ_ERR, strlen(READ_ERR)) != strlen(READ_ERR)) {
+            exit(1);
+        }
+        exit(1);
+    }
+
+    close(old_file_fd);
+    close(new_file_fd);
+    return 0;
+}
