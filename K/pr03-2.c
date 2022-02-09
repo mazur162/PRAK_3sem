c70f78bdcde220e6fc350242744f0251f171b5e0
mazur162 <mazur.anastasia162@gmail.com>
2022-02-09 20:45:29 +0300

    .

---
+++ K/pr03-2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int
+main(void) {
+    int N;
+    scanf("%d", &N);
+    int i, j;
+    j = 1;
+    for (i = j; i < N + 1; i++) {
+        j++;
+        pid_t pid = fork();
+
+        if (pid != 0) {
+            printf("%d", i);
+
+            if (i == N) {
+                printf("\n");
+                return 0;
+            } else {
+                printf(" ");
+                wait(NULL);
+            }
+
+            break;
+            wait(NULL);
+            return 0;
+        }
+    }
+}
