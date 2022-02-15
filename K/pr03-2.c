eb7f254c18896164479cb3c4bfa377ee352c977c
mazur162 <mazur.anastasia162@gmail.com>
2022-02-15 22:06:51 +0300

    upd

--- K/pr03-2.c
+++ K/pr03-2.c
@@ -7,28 +7,5 @@
 
 int
 main(void) {
-    int N;
-    scanf("%d", &N);
-    int i, j;
-    j = 1;
-    for (i = j; i < N + 1; i++) {
-        j++;
-        pid_t pid = fork();
-
-        if (pid != 0) {
-            printf("%d", i);
-
-            if (i == N) {
-                printf("\n");
-                return 0;
-            } else {
-                printf(" ");
-                wait(NULL);
-            }
-
-            break;
-            wait(NULL);
-            return 0;
-        }
-    }
+    
 }
